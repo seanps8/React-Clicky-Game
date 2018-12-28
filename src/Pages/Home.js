498326ff64babbc61a48b227ce2731c02b8bea62
Blazer <sstubbs012@gmail.com>
2018-12-28 10:44:20 -0500

    page is loading correctly

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "../components/Container";
+
+function Home() {
+    return (
+        <div>
+            <Container style= {{ marginTop: 30 }}>
+                <h1>Clicky Game</h1>
+                <h2>Welcome click something</h2>
+            </Container>
+        </div>
+    );
+}
+
+export default Home;
