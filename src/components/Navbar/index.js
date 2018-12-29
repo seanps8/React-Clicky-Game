a2a186f94d69d5a209cfd08bae0eb3a734b93f53
Blazer <sstubbs012@gmail.com>
2018-12-29 11:32:38 -0500

    fixed nav and added team cards

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,12 +4,15 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav>
       <ul>
-        <li className="navbar-brand alignLeft">
-          <a href="#">MLB Clicky Game</a>
+        <li className="brand animated lightSpeedIn">
+          <a href="/react-clicky-game/">MLB Clicky Game</a>
         </li>
-        <li className="nav navbar-nav" id="score">Score: {props.score} | Top Score: {props.topScore}</li>
+
+        <li id="rw">{props.rightWrong}</li>
+
+        <li id="score">Score: {props.score} | Top Score: {props.topScore}</li>
       </ul>
       
       
